3c180a6d1b0febc3a574d32297eb28efe88a1a8c
ibrahim masry <ibrahim.elmansoury@gmail.com>
2022-05-21 04:33:07 +0200

    build frontend before publish

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import "./index.css";
 import "@reach/menu-button/styles.css";
-import socket from "./socket";
 import AppProvider from "./AppProvider";
 
 const container = document.getElementById("root");
