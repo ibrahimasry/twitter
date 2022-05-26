784f5d37e50ec595a210bf3e6994c535bdf0d6a7
ibrahim masry <ibrahim.elmansoury@gmail.com>
2022-05-26 12:38:56 +0200

    add mailgun

--- mail.js
+++ mail.js
@@ -1,20 +1,37 @@
-import sgMail from "@sendgrid/mail";
-sgMail.setApiKey(process.env.EMAIL);
+import mailgun from "mailgun-js"
+// import sgMail from "@sendgrid/mail";
+// sgMail.setApiKey(process.env.EMAIL);
 
-export default async function sendEmail({
-  from = "ibrahim@twitter-clone.com",
-  to,
-  subject,
-  html,
-}) {
-  try {
-    await sgMail.send({
-      from,
-      to,
-      subject,
-      html,
-    });
-  } catch (e) {
-    console.error(e, "error");
-  }
+// export default async function sendEmail({
+//   from = "ibrahim@twitter-clone.com",
+//   to,
+//   subject,
+//   html,
+// }) {
+//   try {
+//     await sgMail.send({
+//       from,
+//       to,
+//       subject,
+//       html,
+//     });
+//   } catch (e) {
+//     console.error(e, "error");
+//   }
+// }
+
+
+const DOMAIN = 'twitter2022.herokuapp.com';
+const mg = mailgun({apiKey: process.env.EMAIL, domain: DOMAIN});
+export default sendEmail({from="ibrahim@twitter.com" , to, subject, html}){
+
+const data = {
+	from,
+	to ,
+	subject,
+	text : html
+};
+mg.messages().send(data, function (error, body) {
+	console.log(body);
+});
 }
