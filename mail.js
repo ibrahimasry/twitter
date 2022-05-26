ef6d9f1e1678f0a909c38c0ea50c0b1c40fcbc12
ibrahim masry <ibrahim.elmansoury@gmail.com>
2022-05-26 13:28:39 +0200

    fix sendgrid

--- mail.js
+++ mail.js
@@ -1,37 +1,42 @@
-import mailgun from "mailgun-js"
-// import sgMail from "@sendgrid/mail";
-// sgMail.setApiKey(process.env.EMAIL);
-
-// export default async function sendEmail({
-//   from = "ibrahim@twitter-clone.com",
-//   to,
-//   subject,
-//   html,
-// }) {
-//   try {
-//     await sgMail.send({
-//       from,
-//       to,
-//       subject,
-//       html,
-//     });
-//   } catch (e) {
-//     console.error(e, "error");
-//   }
-// }
 
 
-const DOMAIN = 'twitter2022.herokuapp.com';
-const mg = mailgun({apiKey: process.env.EMAIL, domain: DOMAIN});
-export default ({from="ibrahim@twitter.com" , to, subject, html}) => {
 
-const data = {
-	from,
-	to ,
-	subject,
-	text : html
-};
-mg.messages().send(data, function (error, body) {
-	console.log(body);
-});
+
+
+// import mailgun from "mailgun-js"
+import sgMail from "@sendgrid/mail";
+sgMail.setApiKey(process.env.EMAIL);
+
+export default async function sendEmail({
+  from = "ibrahim@twitter-clone.com",
+  to,
+  subject,
+  html,
+}) {
+  try {
+    await sgMail.send({
+      from,
+      to,
+      subject,
+      html,
+    });
+  } catch (e) {
+    console.error(e, "error");
+  }
 }
+
+
+// const DOMAIN = 'twitter2022.herokuapp.com';
+// const mg = mailgun({apiKey: process.env.EMAIL, domain: DOMAIN});
+// export default ({from="ibrahim@twitter.com" , to, subject, html}) => {
+
+// const data = {
+// 	from,
+// 	to ,
+// 	subject,
+// 	text : html
+// };
+// mg.messages().send(data, function (error, body) {
+// 	console.log(body);
+// });
+// }
